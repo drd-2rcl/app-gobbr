1cd649b39c3f6e726040f58f4eb0dbf1d33ed222
Diogo <drd.rclrd@gmail.com>
2019-12-15 10:34:54 -0300

    Fix connect db and add env-example

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,8 +1,9 @@
 require('dotenv/config');
 
 module.exports = {
-  dialect: process.env.DB_HOST,
-  host: 'localhost',
+  dialect: 'postgres',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
