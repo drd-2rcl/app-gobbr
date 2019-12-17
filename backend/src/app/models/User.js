1c72aaf5cc0efce07d83357905d9af9938be95da
Diogo <drd.rclrd@gmail.com>
2019-12-17 15:44:20 -0300

    Fix align code

--- backend/src/app/models/User.js
+++ backend/src/app/models/User.js
@@ -10,13 +10,13 @@ class User extends Model {
         password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
         provider: Sequelize.BOOLEAN,
-      }, 
+      },
       {
         sequelize,
       }
     );
 
-    this.addHook('beforeSave', async (user) => {
+    this.addHook('beforeSave', async user => {
       if (user.password) {
         user.password_hash = await bcrypt.hash(user.password, 8);
       }
@@ -25,10 +25,13 @@ class User extends Model {
     return this;
   }
 
+  static associate(models) {
+    this.belongsTo(models.File, { foreignKey: 'avatar_id' });
+  }
+
   checkPassword(password) {
     return bcrypt.compare(password, this.password_hash);
   }
 }
 
 export default User;
-
