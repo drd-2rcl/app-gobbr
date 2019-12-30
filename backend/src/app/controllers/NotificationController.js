e292b6001a77fbed97946d057177c9f087fb19bc
Diogo <drd.rclrd@gmail.com>
2019-12-30 13:42:31 -0300

    Add method to update notification read

--- backend/src/app/controllers/NotificationController.js
+++ backend/src/app/controllers/NotificationController.js
@@ -16,10 +16,20 @@ class NotificationController {
     const notifications = await Notification.find({
       user: req.userId,
     })
-      .sort('createdAt')
+      .sort({ createdAt: 'desc' })
       .limit(20);
 
     return res.json(notifications);
+  }
+
+  async update(req, res) {
+    const notification = await Notification.findByIdAndUpdate(
+      req.params.id,
+      { read: true },
+      { new: true }
+    );
+
+    return res.json(notification);
   }
 }
 
