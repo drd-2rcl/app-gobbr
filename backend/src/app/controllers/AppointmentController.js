471d94c752765d4e64db4cfd987de673310dd772
Diogo <drd.rclrd@gmail.com>
2020-01-08 22:40:52 -0300

    cleanup code

--- backend/src/app/controllers/AppointmentController.js
+++ backend/src/app/controllers/AppointmentController.js
@@ -140,20 +140,6 @@ class AppointmentController {
 
     await appointment.save();
 
-    // await Mail.sendMail({
-    //   to: `${appointment.provider.name} <${appointment.provider.email}>`,
-    //   subject: 'Agendamento cancelado',
-    //   text: 'Você tem um novo cancelamento',
-    //   template: 'cancelation',
-    //   context: {
-    //     provider: appointment.provider.name,
-    //     user: appointment.user.name,
-    //     date: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm'h'", {
-    //       locale: pt,
-    //     }),
-    //   },
-    // });
-
     await Queue.add(CancellationMail.key, {
       appointment,
     });
