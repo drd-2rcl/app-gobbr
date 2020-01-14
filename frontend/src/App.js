3a0f57ab5ba4a971f6240339f60d33352f54567b
Diogo <drd.rclrd@gmail.com>
2020-01-13 22:24:48 -0300

    Add routes in app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React from 'react';
+import { Router } from 'react-router-dom';
+
+import history from './services/history';
+import Routes from './routes';
 
 function App() {
-  return <div>Frontend Gobbr</div>;
+  return (
+    <Router history={history}>
+      <Routes />
+    </Router>
+  );
 }
 
 export default App;
