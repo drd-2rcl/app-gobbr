c1db2f32110f980fbc469765415004d7a8f5ba1d
Diogo <drd.rclrd@gmail.com>
2020-01-25 19:40:42 -0300

    Change address to root import

--- frontend/src/routes/Route.js
+++ frontend/src/routes/Route.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-import AuthLayout from '../pages/_layouts/auth';
-import DefaultLayout from '../pages/_layouts/default';
+import AuthLayout from '~/pages/_layouts/auth';
+import DefaultLayout from '~/pages/_layouts/default';
 
 export default function RouteWrapper({
   component: Component,
