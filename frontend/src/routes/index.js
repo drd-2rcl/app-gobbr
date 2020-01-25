349b05de179bb75e45e2f52915d724f385648961
Diogo <drd.rclrd@gmail.com>
2020-01-25 13:40:53 -0300

    Change to customize private Route

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch } from 'react-router-dom';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
+import Route from './Route';
 
 export default function Routes() {
   return (
@@ -12,8 +13,9 @@ export default function Routes() {
       <Switch>
         <Route path="/" exact component={SignIn} />
         <Route path="/register" exact component={SignUp} />
-        <Route path="/dashboard" exact component={Dashboard} />
-        <Route path="/profile" exact component={Profile} />
+        <Route path="/dashboard" exact component={Dashboard} isPrivate />
+        <Route path="/profile" exact component={Profile} isPrivate />
+        <Route path="/" component={() => <h1>404 Not Found</h1>} />
       </Switch>
     </BrowserRouter>
   );
