0f6d5c37b617291e43203af68149bbc0659d1514
Diogo <drd.rclrd@gmail.com>
2020-01-25 14:26:27 -0300

    Create layouts to reuse

---
+++ frontend/src/pages/_layouts/auth/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function AuthLayout({ children }) {
+  return (
+    <div>
+      <Container></Container>
+    </div>
+  );
+}
---
+++ frontend/src/pages/_layouts/default/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function DefaultLayout({ children }) {
+  return (
+    <div>
+      <Container></Container>
+    </div>
+  );
+}
