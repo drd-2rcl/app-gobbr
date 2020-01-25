97b2d83f9d1053b1b024d8dbdb1afe989de60e1d
Diogo <drd.rclrd@gmail.com>
2020-01-25 19:24:20 -0300

    Add global style in app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,13 @@ import { Router } from 'react-router-dom';
 import history from './services/history';
 import Routes from './routes';
 import './config/ReactotronConfig';
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <Router history={history}>
       <Routes />
+      <GlobalStyle />
     </Router>
   );
 }
