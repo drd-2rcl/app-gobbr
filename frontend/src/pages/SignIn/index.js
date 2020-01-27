067b1105e95a260e2f8f678181e63a975b4c07b9
Diogo <drd.rclrd@gmail.com>
2020-01-26 23:41:01 -0300

    Add structure to register and login

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
-  return <div>SignIn</div>;
+  return (
+    <>
+      <img src={logo} alt="Logo" />
+      <form>
+        <input type="email" placeholder="Seu email" />
+        <input type="password" placeholder="Sua senha secreta" />
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </form>
+    </>
+  );
 }
