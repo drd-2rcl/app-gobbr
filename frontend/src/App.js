30c01632cf5b33d2b62ca2486641d16ff662e815
Diogo <drd.rclrd@gmail.com>
2020-01-28 11:19:49 -0300

    Add provider react-redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import history from './services/history';
 import Routes from './routes';
 import './config/ReactotronConfig';
 import GlobalStyle from './styles/global';
+import store from './store';
 
 function App() {
   return (
-    <Router history={history}>
-      <Routes />
-      <GlobalStyle />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyle />
+      </Router>
+    </Provider>
   );
 }
 
