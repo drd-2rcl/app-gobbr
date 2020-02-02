4a74449026fbeb2b21078fc7915a8b218e11f68a
Diogo <drd.rclrd@gmail.com>
2020-02-02 12:53:38 -0300

    Add redux persist in app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
@@ -6,15 +7,17 @@ import './config/ReactotronConfig';
 import history from './services/history';
 import Routes from './routes';
 import GlobalStyle from './styles/global';
-import store from './store';
+import { store, persistor } from './store';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyle />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
